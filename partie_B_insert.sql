90da3ec2573c105fc2d3e8a13afca73e386ff02b
Arthur MAGNIER <armagnier>
2025-01-15 12:16:49 +0100

    Fin partie B

--- partie_B_insert.sql
+++ partie_B_insert.sql
@@ -1,14 +1,14 @@
-\copy dCategorie(idCategorie, nomCategorie) from 'P:/Bureau/5A/Entrepot de donnees/polytech2024edd_suj_source/tb_categorie_data.csv' DELIMITER ',' CSV HEADER;
+\copy dCategorie(idCategorie, nomCategorie) from 'P:/Bureau/5A/dwh-course-practice/polytech2024edd_suj_source/tb_categorie_data.csv' DELIMITER ',' CSV HEADER;
 
 INSERT INTO dModele(idModele, nomModele, idCategorie)
 SELECT idModele, nomModele, idCategorie
 FROM Modele;
 
-\copy dPays(idPays, nomPays, supPays) from 'P:/Bureau/5A/Entrepot de donnees/polytech2024edd_suj_source/tb_pays_data.csv' DELIMITER ',' CSV HEADER;
+\copy dPays(idPays, nomPays, supPays) from 'P:/Bureau/5A/dwh-course-practice/polytech2024edd_suj_source/tb_pays_data.csv' DELIMITER ',' CSV HEADER;
 
-\copy dVille(idVille, nomVille, supVille, idPays) from 'P:/Bureau/5A/Entrepot de donnees/polytech2024edd_suj_source/tb_ville_data.csv' DELIMITER ',' CSV HEADER;
+\copy dVille(idVille, nomVille, supVille, idPays) from 'P:/Bureau/5A/dwh-course-practice/polytech2024edd_suj_source/tb_ville_data.csv' DELIMITER ',' CSV HEADER;
 
-\copy dMagasin(idMagasin, nomMagasin, idVille) from 'P:/Bureau/5A/Entrepot de donnees/polytech2024edd_suj_source/tb_magasin_data.csv' DELIMITER ',' CSV HEADER;
+\copy dMagasin(idMagasin, nomMagasin, idVille) from 'P:/Bureau/5A/dwh-course-practice/polytech2024edd_suj_source/tb_magasin_data.csv' DELIMITER ',' CSV HEADER;
 
 INSERT INTO fNbProduitsVendus(idTemps, idMagasin, idModele, montant, quantite)
 SELECT v.dateVente AS idTemps, v.idMagasin, m.idModele, SUM(vp.prixVente) AS montant, COUNT(vp.idProduit) AS quantite
